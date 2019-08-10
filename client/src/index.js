2b99e727d87e1babc1bc577eeac6a69b6f52bba1
Ikhtiar <ikhtiaralam@gmail.com>
2019-08-10 11:20:14 -0500

    index.js changes

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { combineReducers } from "redux";
-import authReducer from "./authReducer";
-import errorReducer from "./errorReducer";
+
+
 
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-export default combineReducers({
-    auth: authReducer,
-    errors: errorReducer
-  });
