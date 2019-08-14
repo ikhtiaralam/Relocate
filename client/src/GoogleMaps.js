f09c589f47f558446dc249c44c4eb110a3e3bde0
ericktorrs1 <ericktorrs90.et@gmail.com>
2019-08-14 18:33:46 -0500

    changes

--- client/src/GoogleMaps.js
+++ client/src/GoogleMaps.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"; 
+import './App.css'; 
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 
@@ -6,17 +7,13 @@ import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 const url = new URL('https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice');
 
 let queryParams = {
-    bedrooms: '3',
-    bathrooms: '2',
-    propertyType: 'Single Family',
-    squareFootage: '1600',
-    compCount: '5',
-    address: '6425 Boaz Lane Dallas TX 75205',
+  bedrooms: '3',
+  bathrooms: '2',
+  propertyType: 'Single Family',
+  squareFootage: '1600',
+  compCount: '5',
+  address: '6425 Boaz Lane Dallas TX 75205',
 };
-
-url.search = new URLSearchParams(queryParams);
-
-
 
 
 const mapStyles = {
@@ -31,40 +28,46 @@ const mapStyles = {
 export class MapContainer extends Component {
     constructor(props) {
       super(props);
+      this.handleSubmit = this.handleSubmit.bind(this);
+      
 
-
+      // TO DO:   
+      //Put Buttons here
 
         this.state = {
         apartments: [],
+        address: []
       };
     }
-
-
+    
+    handleSubmit = event => {
+    // event.preventDefault();
+    const data = event.target.value;
+    console.log("The user input: " + data);
+    queryParams.address = data; 
+   
+      }
 
     componentDidMount() {
-        fetch(url, {
-            headers: {
-                'x-rapidapi-host': 'realtymole-rental-estimate-v1.p.rapidapi.com',
-                'x-rapidapi-key': 'xjePNjcZoBmshIkdUz40QcJfpWSwp1cIRSWjsnxVLZj2JW7Y3X',
-            },
+      url.search = new URLSearchParams(queryParams);
+      
+      fetch(url, {
+        headers: {
+            'x-rapidapi-host': 'realtymole-rental-estimate-v1.p.rapidapi.com',
+            'x-rapidapi-key': 'xjePNjcZoBmshIkdUz40QcJfpWSwp1cIRSWjsnxVLZj2JW7Y3X',
+        },
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            // SET STATE HERE
+            this.setState({ 
+                apartments:res.listings,
+            });
         })
-            .then(res => res.json())
-            .then(res => {
-                console.log(res);
-                // SET STATE HERE
-                this.setState({ 
-                    apartments:res.listings,
-                });
-            })
-            .catch(err => console.log(err));
+        .catch(err => console.log(err));
     }
-
-
-
-
-
-
-  
+    
     displayMarkers = () => {
       return this.state.apartments.map((store, index) => {
         return <Marker key={index} id={index} position={{
@@ -77,6 +80,19 @@ export class MapContainer extends Component {
 
     render() {
       return (
+        <div>
+
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="workaddress">Work Address: </label>
+          <input className="search"
+              type= 'text'
+              placeholder='6425 Boaz Lane Dallas TX 75205'
+              name="workaddress"
+              ref={(input) => this.input = input}
+              />
+              <button className="searchButton">Search</button>
+          </form>
+
           <Map
             google={this.props.google}
             zoom={12}
@@ -85,12 +101,14 @@ export class MapContainer extends Component {
           >
             {this.displayMarkers()}
           </Map>
+          
+          
+          
+          </div> 
 
       );
     }
   }
-
-
 
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyB9MfOgxe8X4br2Y7F4hrTaLH5WjirWI4E'
